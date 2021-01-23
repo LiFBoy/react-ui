67a755e354dd2d6aa61b1b30b78b51782b5ec45e
ljb <1244659255@qq.com>
2021-01-23 15:37:08 +0800

    fix: 1

--- components/alter/demo/1-demo-basic.tsx
+++ components/alter/demo/1-demo-basic.tsx
@@ -1,44 +1,10 @@
 import React from 'react';
 import Alert from 'happy-ui/lib/alter';
-// import SelectUser, { PropTypes } from 'happy-ui/src/index';
 import 'happy-ui/lib/alter/style';
 
 export default () => {
-  const show = () => {
-    SelectUser.show({
-      multiple: true,
-      // selectType: 'dept',
-      dialogProps: {
-        title: '选人组件',
-      },
-      selectPaneProps: {
-        dept: {
-          title: '班级',
-        },
-      },
-      requestParams: {
-        // campusType: 'base_school_type',
-        // deptTypeList: ['class'],
-        strictUser: true,
-        selectTypeList: ['user', 'dept', 'org', 'group', 'tags'],
-      },
-      // isSaveSelectSignature: true,
-      // selectSignature: '7c5d12fe8d3c4d819620dff496c9e38b-v4-330108-S000067',
-      showTabList: ['dept', 'innerContacts', 'tags'],
-      // showTabList: ['schoolContacts'],
-      onOk: () => {},
-      onCancel: () => {},
-      getCheckedNodes(data: any) {
-        console.log('getCheckedNodes data', data);
-      },
-      getTotalCount(data: any) {
-        console.log('getTotalCount data', data);
-      },
-    });
-  };
   return (
     <div>
-      <div>{/* <div onClick={show}>唤起选人组件</div> */}</div>
       <Alert kind="warning">eeeee</Alert>
     </div>
   );
